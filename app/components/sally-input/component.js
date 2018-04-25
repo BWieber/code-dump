076e6fc4109ab3614fc4cee63e0b5c0dbf4fc19c
Brandon Wieber <brandon.m.wieber@gmail.com>
2018-04-24 20:28:18 -0400

    ember scaffold

---
+++ app/components/sally-input/component.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+	// constructor() {
+	// 	//        this.errorMessageEle = document.getElementById("error");
+	// 	this.reportMessageEle = document.getElementById("report");
+	// }
+  //
+	// renderReport() {
+	// 	var currentRobot = window.simulator.getCurrentRobot();
+	// 	this.reportMessageEle.innerHTML = '<span>' + 'Axis X: ' + currentRobot.x + '</span>' +
+	// 		'<span>' + 'Axis Y: ' + currentRobot.y + '</span>' +
+	// 		'<span>' + 'Facing: ' + currentRobot.f + '</span>';
+	// }
+  //
+	// renderErrors(msg) {
+	// 	this.reportMessageEle.innerHTML = '<span id="error">' + msg + '</span>';
+	// }
+  //
+	// clear() {
+	// 	this.reportMessageEle.innerHTML = '';
+	// 	//        this.errorMessageEle.innerHTML = '';
+	// }
+});
